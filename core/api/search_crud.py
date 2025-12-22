0c31307225586fab68a70c79ec74af7732e941e1
Krajew <mateusz.krajewskii@icloud.com>
2025-12-22 20:02:57 +0100

    flake8 formatting and changes in searching

--- core/api/search_crud.py
+++ core/api/search_crud.py
@@ -4,6 +4,7 @@ from core.services.queries_service.search_queries import SearchQueries
 router = APIRouter(prefix="/search", tags=["Search"])
 service = SearchQueries()
 
+
 @router.get("/")
 async def get_search_results(q: str = Query(..., min_length=1)):
     """
--- core/services/queries_service/search_queries.py
+++ core/services/queries_service/search_queries.py
@@ -2,35 +2,33 @@ from sqlalchemy import or_
 from database.models import Applicant, Job, Company
 from database.database import get_db_session
 
+
 class SearchQueries:
     def __init__(self):
         self.db = get_db_session()
 
     def global_search(self, query: str):
-        search_filter = f"%{query}%"
-        
+        search_filter = f"{query}%"
+
         # Ludzie
         people = self.db.query(Applicant).filter(
             or_(
                 Applicant.name.ilike(search_filter),
-                Applicant.surname.ilike(search_filter),
-                Applicant.description.ilike(search_filter)
+                Applicant.surname.ilike(search_filter)
             )
         ).all()
 
         # Oferty pracy
         jobs = self.db.query(Job).filter(
             or_(
-                Job.title.ilike(search_filter),
-                Job.description.ilike(search_filter)
+                Job.title.ilike(search_filter)
             )
         ).all()
 
         # Firmy
         companies = self.db.query(Company).filter(
             or_(
-                Company.name.ilike(search_filter),
-                Company.description.ilike(search_filter)
+                Company.name.ilike(search_filter)
             )
         ).all()
 
